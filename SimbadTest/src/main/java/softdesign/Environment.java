3393078afca4903ce2a3c0ca115a401efb302506
Hasrh <97hknl@gmail.com>
2018-02-26 20:07:41 +0100

    Harsh

--- SimbadTest/src/main/java/softdesign/Environment.java
+++ SimbadTest/src/main/java/softdesign/Environment.java
@@ -13,14 +13,13 @@ import simbad.sim.EnvironmentDescription;
 import simbad.sim.Wall;
 
 public class Environment extends EnvironmentDescription {
-	ArrayList<Wall> walls;
-	int worldSize = 20;
+	int worldSize;
 	public Environment() {
-		walls = new ArrayList<Wall>();
+		
 		// turn on the lights
         this.light1IsOn = true;
         this.light2IsOn = true;
-        
+        this.worldSize = 20;
         // enable the physics engine in order to have better physics effects on the objects
         this.setUsePhysics(true);
         
@@ -33,40 +32,34 @@ public class Environment extends EnvironmentDescription {
         w1.setColor(new Color3f(Color.BLUE));
         w1.rotate90(1);
         add(w1);
-        walls.add(w1);
         
         Wall w2 = new Wall(new Vector3d(5, 0, 0), 10, 2, this);
-        w2.setColor(new Color3f(Color.BLUE));
+        w2.setColor(new Color3f(Color.RED));
         w2.rotate90(1);
         add(w2);
-        walls.add(w2);
         
         Wall w3 = new Wall(new Vector3d(0, 0, 5), 10, 2, this);
-        w3.setColor(new Color3f(Color.BLUE));
+        w3.setColor(new Color3f(Color.GREEN));
         add(w3);
-        walls.add(w3);
         
         Wall w4 = new Wall(new Vector3d(0, 0, -5), 10, 2, this);
-        w4.setColor(new Color3f(Color.BLUE));
+        w4.setColor(new Color3f(Color.YELLOW));
         add(w4);
-        walls.add(w4);
 
         Box box1 = new Box(new Vector3d(-3, 0, -3), new Vector3f(1, 1, 1), this);
-        box1.setColor(new Color3f(Color.YELLOW));
+        box1.setColor(new Color3f(Color.BLACK));
         add(box1);
         
         Box box2 = new Box(new Vector3d(-3, 0, 3), new Vector3f(1, 1, 1), this);
-        box2.setColor(new Color3f(Color.YELLOW));
+        box2.setColor(new Color3f(Color.ORANGE));
         add(box2);
         
     }
 	
-	//public boolean isInBounds
-	
 	public Hashtable<Vector3d, String> getCoordinatesInArea(){
 		Hashtable<Vector3d, String> result = new Hashtable<>();
-		for(double i=0; i<worldSize; i+=0.05) {
-			for(double j=0; j<worldSize; j+=0.05) {
+		for(double i=-worldSize; i<worldSize; i+=0.05) {
+			for(double j=-worldSize; j<worldSize; j+=0.05) {
 				result.put(new Vector3d(i, 0, j), "unknown");
 			}
 		}
