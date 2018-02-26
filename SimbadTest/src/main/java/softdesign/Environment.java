0cedaeb602060961dadcb676d26849983a5de06c
Hasrh <97hknl@gmail.com>
2018-02-26 12:20:11 +0100

    Harsh

---
+++ SimbadTest/src/main/java/softdesign/Environment.java
@@ -0,0 +1,75 @@
+package main.java.softdesign;
+
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.Hashtable;
+
+import javax.vecmath.Color3f;
+import javax.vecmath.Vector3d;
+import javax.vecmath.Vector3f;
+
+import simbad.sim.Box;
+import simbad.sim.EnvironmentDescription;
+import simbad.sim.Wall;
+
+public class Environment extends EnvironmentDescription {
+	ArrayList<Wall> walls;
+	int worldSize = 20;
+	public Environment() {
+		
+		// turn on the lights
+        this.light1IsOn = true;
+        this.light2IsOn = true;
+        
+        // enable the physics engine in order to have better physics effects on the objects
+        this.setUsePhysics(true);
+        
+        // show the axes so that we know where things are
+        this.showAxis(true);
+        
+        this.setWorldSize(worldSize);
+        
+        Wall w1 = new Wall(new Vector3d(-5, 0, 0), 10, 2, this); //x, z, y form, length and height. Nothing for thickness
+        w1.setColor(new Color3f(Color.BLUE));
+        w1.rotate90(1);
+        add(w1);
+        walls.add(w1);
+        
+        Wall w2 = new Wall(new Vector3d(5, 0, 0), 10, 2, this);
+        w2.setColor(new Color3f(Color.BLUE));
+        w2.rotate90(1);
+        add(w2);
+        walls.add(w2);
+        
+        Wall w3 = new Wall(new Vector3d(0, 0, 5), 10, 2, this);
+        w3.setColor(new Color3f(Color.BLUE));
+        add(w3);
+        walls.add(w3);
+        
+        Wall w4 = new Wall(new Vector3d(0, 0, -5), 10, 2, this);
+        w4.setColor(new Color3f(Color.BLUE));
+        add(w4);
+        walls.add(w4);
+
+        Box box1 = new Box(new Vector3d(-3, 0, -3), new Vector3f(1, 1, 1), this);
+        box1.setColor(new Color3f(Color.YELLOW));
+        add(box1);
+        
+        Box box2 = new Box(new Vector3d(-3, 0, 3), new Vector3f(1, 1, 1), this);
+        box2.setColor(new Color3f(Color.YELLOW));
+        add(box2);
+        
+    }
+	
+	//public boolean isInBounds
+	
+	public Hashtable<Vector3d, String> getCoordinatesInArea(){
+		Hashtable<Vector3d, String> result = new Hashtable<>();
+		for(double i=0; i<worldSize; i+=0.05) {
+			for(double j=0; j<worldSize; j+=0.05) {
+				result.put(new Vector3d(i, 0, j), "unknown");
+			}
+		}
+		return result;
+	}
+}
---
+++ SimbadTest/src/main/java/softdesign/Main.java
@@ -0,0 +1,59 @@
+package main.java.softdesign;
+
+import simbad.gui.*;
+import simbad.sim.*;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+
+import javax.vecmath.Point3d;
+import javax.vecmath.Vector3d;
+
+/**
+ *
+ * Start working on this file
+ *
+ * You can use as many source files and packages
+ * as you like, as long as everything is below
+ * main.java.softdesign
+ *
+ * Some resources:
+ *
+ * - https://github.com/jimmikaelkael/simbad/tree/master/src
+ * - https://www.ibm.com/developerworks/java/library/j-robots/
+ * - https://github.com/VU-SoftwareDesign/SimbadMultiRobot
+ * - https://github.com/VU-SoftwareDesign/SimbadHelloWorld
+ *
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        // request antialising so that diagonal lines are not "stairy"
+        System.setProperty("j3d.implicitAntialiasing", "true");
+        
+        // creation of the environment containing all obstacles and robots
+        Environment environment = new Environment();
+        
+        // create two instances of the same example robot
+        Robot robot1 = new Robot(new Vector3d(0, 0, 0), "Robot 1");
+        Robot robot2 = new Robot(new Vector3d(-4, 0, -4), "Robot 2");
+        Robot robot3 = new Robot(new Vector3d(4, 0, -4), "Robot 3");
+        Robot robot4 = new Robot(new Vector3d(-4, 0, 4), "Robot 4");
+        
+      // add the four robots to the environment
+        environment.add(robot1);
+        environment.add(robot2);
+        environment.add(robot3);
+        environment.add(robot4);
+        
+        Hashtable<Vector3d, String >worldMap = environment.getCoordinatesInArea();
+        
+        // here we create an instance of the whole Simbad simulator and we assign the newly created environment 
+        Simbad frame = new Simbad(environment, false);
+        frame.update(frame.getGraphics());
+        
+//        todo:
+//        	-add a while loop to continiously get pictures from robots and terminate if every coordinate in worldmap is marked
+    }
+
+} 
