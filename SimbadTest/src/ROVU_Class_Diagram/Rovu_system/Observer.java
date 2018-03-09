124279e6bab58aebcaf8f5d30dabecccb9d52f47
Evermeij <ewoudvermeij@gmail.com>
2018-03-09 12:59:16 +0100

    Changes so far

--- SimbadTest/src/ROVU_Class_Diagram/Rovu_system/Observer.java
+++ SimbadTest/src/ROVU_Class_Diagram/Rovu_system/Observer.java
@@ -22,6 +22,6 @@ public abstract class Observer extends Agent{
 	}
 
 
-	public abstract void update();
+	public abstract void update(Task task);
 
 };
--- SimbadTest/src/ROVU_Class_Diagram/Rovu_system/Robot.java
+++ SimbadTest/src/ROVU_Class_Diagram/Rovu_system/Robot.java
@@ -31,8 +31,10 @@ public class Robot extends Observer {
 	private RangeSensorBelt sonars;
 	private CameraSensor camera;
 	private boolean lastTurnLeft;
+	private boolean wallFolowing;
+	private boolean goingHome;
 	
-	private final Enum[] direction = {RobotDirection.NORTH, RobotDirection.WEST, RobotDirection.SOUTH, RobotDirection.EAST}; 
+	private final RobotDirection[] direction = {RobotDirection.NORTH, RobotDirection.WEST, RobotDirection.SOUTH, RobotDirection.EAST}; 
 	private int currentDirection;
 	private boolean sendCoordinatesWithInterval;
 
@@ -51,17 +53,19 @@ public class Robot extends Observer {
 		
 	}
 	
-	public void update() {
-		Task toExecute = subject.getTask();
+	public void update(Task toExecute) {
 		executeTask(toExecute);
 	}
 	
 	private void executeTask(Task executable) {
 		switch(executable.request) {
-			case turnLeft: turnLeft();
+			case turnLeft: 
+				turnLeft();
+				wallFolowing = false;
 			break;
 			
 			case turnRight: turnRight();
+				wallFolowing = false;
 			break;
 			
 			case sendCoordinates: sendCoordinates();
@@ -71,17 +75,23 @@ public class Robot extends Observer {
 				this.sendCoordinatesWithInterval = true;
 				break;
 			
+			case goHome:
+				this.moveToStartPosition();
+				goingHome = true;
+			
 			default: System.out.printf("Rover %s got an unkown task\n",this.name);
 				break;
 		}
 	}
 	
 	private void sendCoordinates() {
-		this.station.reportCoordinate((int) getCurrentCoordinate()[0] + 9, (int) getCurrentCoordinate()[1] + 9);
+		this.station.report((int) getCurrentCoordinate()[0] + 9, (int) getCurrentCoordinate()[1] + 9, this, direction[currentDirection], sonars.getMeasurement(6), wallFolowing);
 	}
 	
 	public void initBehavior() {
         System.out.println("I exist and my name is " + this.name);
+        wallFolowing = true;
+        goingHome = false;
 	}
 
 	/**
@@ -101,20 +111,30 @@ public class Robot extends Observer {
 		currentDirection = direc;
 	}
 	
+	public RobotDirection getDirection() {
+		return direction[currentDirection];
+	}
+	
 	public void turnLeft() {
+		lastTurnLeft = true;
 		if(currentDirection == 3) {
 			currentDirection = 0;
 		}else {
 			currentDirection++;
 		}
+		this.setTranslationalVelocity(0.0);
+		this.rotateY(Math.PI/2);
 	}
 	
 	public void turnRight() {
+		lastTurnLeft = false;
 		if(currentDirection == 0) {
 			currentDirection = 3;
 		}else {
 			currentDirection--;
 		}
+		this.setTranslationalVelocity(0.0);
+		this.rotateY(Math.PI / 2 * 3);
 	}
 
 	/**
@@ -122,41 +142,37 @@ public class Robot extends Observer {
 	 */
 	public void performBehavior() {
 		// perform the following actions every 7 virtual seconds
-    	if(this.getCounter() % 6 == 0) {
-    		if(lastTurnLeft) {
-    			if(sonars.getMeasurement(5) > 0.5 && sonars.getMeasurement(6) > 1 && sonars.getMeasurement(7) > 1.5) {
-    				turnRight();
-    				lastTurnLeft = false;
-        			this.setTranslationalVelocity(0.0);
-        			this.rotateY(Math.PI / 2 * 3);
-        			System.out.println(this.name + " driving " + direction[currentDirection]);
-        		}
-    		}else{
-    			if(sonars.getMeasurement(5) > 0.9 && sonars.getMeasurement(6) > 1 && sonars.getMeasurement(7) > 1.5) {
-    				turnRight();
-    				lastTurnLeft = false;
-        			this.setTranslationalVelocity(0.0);
-        			this.rotateY(Math.PI / 2 * 3);
-        			System.out.println(this.name + " driving " + direction[currentDirection]);
-        		}
-    		}
-    		
-    		if(sonars.getMeasurement(0) < 0.28) {
-    			turnLeft();
-    			lastTurnLeft = true;
-    			this.setTranslationalVelocity(0.0);
-    			this.rotateY(Math.PI/2);
-    			System.out.println(this.name + " driving " + direction[currentDirection]);
-    		}else {
-    			this.setTranslationalVelocity(0.5);
-    		}
+		if(!goingHome) {
+			if(this.getCounter() % 6 == 0) {
+	    		if(lastTurnLeft && wallFolowing) {
+	    			if(sonars.getMeasurement(5) > 0.5 && sonars.getMeasurement(6) > 1 && sonars.getMeasurement(7) > 1.5) {
+	    				turnRight();
+	        		}
+	    		}else{
+	    			if(sonars.getMeasurement(5) > 0.9 && sonars.getMeasurement(6) > 1 && sonars.getMeasurement(7) > 1.5 && wallFolowing) {
+	    				turnRight();
+	        		}
+	    		}
+	    		
+	    		if(sonars.getMeasurement(0) < 0.28) {
+	    			wallFolowing = true;
+	    			turnLeft();
+	    		}else {
+	    			this.setTranslationalVelocity(0.5);
+	    		}
+	    	}
+	    	if(sendCoordinatesWithInterval == true) {
+	    		if(this.getCounter() % 10 == 0) {
+	        		//station.report(this);
+	        		sendCoordinates();
+	        	}
+	    	}
+		}
+    	
+    	
+    	if(this.getCounter() % 100 == 0) {
+    		System.out.println("Percentage: " + station.map.getCoveredPercentage());
     	}
-    	if(sendCoordinatesWithInterval == true) {
-    		if(this.getCounter() % 20 == 0) {
-        		//station.report(this);
-        		sendCoordinates();
-        	}
-    	}
-    	System.out.printf("Percentage: %.2f%%\n",station.map.getCoveredPercentage());
+    	
 	}
 };
